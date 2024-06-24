496c8cef99f1d8348a98b7697d52b9687aed4f0e
Joan Benaiges <jobeto87@gmail.com>
2024-06-24 14:16:09 +0100

    navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,12 +13,20 @@ function Navigation({ language, toggleLanguage }) {
             <nav>
                 <div className='logo'></div>
                 <div className="nav-container">
-                    <div className="menu-icon" onClick={toggleMenu}>☰</div>
+
+                    <button className="menu-icon" onClick={toggleMenu}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </button>
+
+                    {/* <div className="menu-icon" onClick={toggleMenu}>☰</div> */}
+
                     <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
                         <li><Link to='/'>{language === 'en' ? 'Home' : 'Inicio'}</Link></li>
                         <li><Link to='/about-me'>{language === 'en' ? 'About Us' : 'Sobre nosotros'}</Link></li>
                         <li><Link to='/projects'>{language === 'en' ? 'Our products' : 'Nuestros productos'}</Link></li>
-                        <li><Link to='/contact-me'>{language === 'en' ? 'Contact Us' : 'Contáctame'}</Link></li>
+                        <li><Link to='/contact-me'>{language === 'en' ? 'Contact Us' : 'Contactanos'}</Link></li>
                     </ul>
                 </div>
                 <button className={`language-btn ${language === 'en' ? 'english' : 'spanish'}`} onClick={toggleLanguage}>
